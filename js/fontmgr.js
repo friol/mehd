b961f73ae14e6b3f0643dc62a55574e355fa95ef
friol <dantonag@gmail.com>
2020-01-03 17:15:13 +0100

    Speed optimizations when writing reverse chars.

--- js/fontmgr.js
+++ js/fontmgr.js
@@ -13,6 +13,62 @@ class fontmgr
 
         this.numLettersX=18;
         this.numLettersY=6;
+
+        //
+
+        this.reverseCanvasArray=[];
+        this.initReverseCanvasArray();
+    }
+
+    initReverseCanvasArray()
+    {
+        var img=document.getElementById("fontImage");
+
+        for (var l=0;l<this.numLettersX*this.numLettersY;l++)
+        {
+            var realPosx=l%this.numLettersX;
+            var realPosy=Math.floor(l/this.numLettersX);
+
+            var cvs = document.createElement('canvas');
+            cvs.width = this.fontwidth+this.multiplier; 
+            cvs.height = this.fontheight+this.multiplier;
+            var ctx = cvs.getContext("2d");
+
+            var idt1 = ctx.getImageData(0,0,cvs.width,cvs.height);
+            var data1 = idt1.data;
+
+            for (var p=0;p<data1.length;p+=4)
+            {
+                data1[p+0]=131; data1[p+1]=255; data1[p+2]=199; data1[p+3]=255;
+            }
+            ctx.putImageData(idt1,0,0);
+
+            ctx.drawImage(img,realPosx*this.fontwidth+(this.lettersShiftX*realPosx),realPosy*this.fontheight,
+                            this.fontwidth,this.fontheight,
+                            0,0,
+                            this.fontwidth,this.fontheight);
+            var idt = ctx.getImageData(0,0,cvs.width-this.multiplier,cvs.height-this.multiplier);
+            var data = idt.data;
+
+            for (var p=0;p<data.length;p+=4)
+            {
+                var r=data[p+0];
+                var g=data[p+1];
+                var b=data[p+2];
+
+                if ((r==25)&&(g==67)&&(b==43))
+                {
+                    data[p+0]=131; data[p+1]=255; data[p+2]=199; data[p+3]=255;
+                }
+                else
+                {
+                    data[p+0]=25; data[p+1]=67; data[p+2]=43; data[p+3]=255;
+                }
+            }
+
+            ctx.putImageData(idt,0,0);
+            this.reverseCanvasArray.push(cvs);
+        }
     }
 
     drawCursor(cx,cy,ctick,csteps,inverted)
@@ -63,6 +119,7 @@ class fontmgr
         }
         else
         {
+            /*
             var cvs = document.createElement('canvas');
             cvs.width = this.fontwidth+this.multiplier; 
             cvs.height = this.fontheight+this.multiplier;
@@ -101,6 +158,9 @@ class fontmgr
             }
 
             ctx.putImageData(idt,0,0);
+            */
+
+            var cvs=this.reverseCanvasArray[charIndex];
             context.drawImage(cvs,px,py);
         }
     }
